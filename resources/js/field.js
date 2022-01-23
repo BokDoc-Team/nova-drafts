c51fa54000db3647ef115f7ec236c12e8964fd7f
MohamedFayed <32162186+MohamedFayed22@users.noreply.github.com>
2022-01-23 13:21:55 +0200

    Test add file form to publish

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,6 +1,7 @@
 import DetailNovaDraftButton from './nova-draft-button/components/DetailButton';
 import FormNovaDraftButton from './nova-draft-button/components/FormButton';
 import IndexNovaPublishedField from './nova-published-field/components/IndexField';
+import FormNovaPublishedField from './nova-published-field/components/FormButton';
 import DetailNovaPublishedField from './nova-published-field/components/DetailButton';
 import DetailNovaUnpublishButton from './nova-unpublish-button/Detail';
 import FormNovaUnpublishButton from './nova-unpublish-button/Form';
@@ -11,7 +12,9 @@ Nova.booting((Vue, router, store) => {
 
   Vue.component('index-nova-published-field', IndexNovaPublishedField);
   Vue.component('detail-nova-published-field', DetailNovaPublishedField);
+  Vue.component('form-nova-published-button', FormNovaPublishedField);
 
-  Vue.component('detail-nova-unpublish-button', DetailNovaUnpublishButton);
+
+    Vue.component('detail-nova-unpublish-button', DetailNovaUnpublishButton);
   Vue.component('form-nova-unpublish-button', FormNovaUnpublishButton);
 });
